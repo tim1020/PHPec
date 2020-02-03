046dc91407ad2f2e250fe2afe27598175c43a54a
Tim <huangtiancai@bcb100.cn>
2020-02-03 12:20:13 +0800

    重新修正Router基类

--- src/middleware/Router.php
+++ src/middleware/Router.php
@@ -3,10 +3,17 @@ namespace PHPec\middleware;
 use PHPec\Middleware;
 
 class Router extends Middleware{
-    function handle() {
-        $pos = strpos($_SERVER['REQUEST_URI'], '?');
-        $key = (false === $pos) ? $_SERVER['REQUEST_URI'] : substr($_SERVER['REQUEST_URI'], 0, $pos);
-        $this -> Request -> setRouterPath($key);
+    final function handle() {
+        $path = $this -> path();
+        $map  = $this -> config('router.map', []);
+        $path = $map[$path] ?? $path;
+        $this -> Request -> setRouterPath($path);
         $this -> next(); 
+    }
+
+    public function path(){
+        $pos = strpos($_SERVER['REQUEST_URI'], '?');
+        $path = (false === $pos) ? $_SERVER['REQUEST_URI'] : substr($_SERVER['REQUEST_URI'], 0, $pos);
+        return $path;
     }
 }
